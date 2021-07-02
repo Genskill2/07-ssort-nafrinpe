c0c399871ccbfc14b51c27ecb6055811a1806bdd
nafrinpe <84736105+nafrinpe@users.noreply.github.com>
2021-07-02 15:24:51 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,62 +1,20 @@
-#include <stdio.h>
-#include <assert.h>
-#include<math.h>
-#include <string.h>
-#include <cs50.h>
-
-string ari(string);
-
-string ari(string s)
-{
-int ch=0;
-int w=0;
-int sent=0;
-int read;
-for(int i=0;s[i]!='\0';i++)
- {
-   if(isalnum(s[i]))
-    {
-    ch++;
+void swap_max(int x[],int l,int y){
+  int largest = 0;
+  int *t=&x[y];
+  int *q;
+  for(int i=y;i<l;i++){
+   if(x[i]>largest){
+    largest=x[i];
+    q=&x[i];
     }
-   if(s[i]==' ')
-    {
-    w++;
+   else
+    continue;
     }
-   if(s[i]=='.'||s[i]=='!'||s[i]=='?')
-    {
-    sent++;
+    int n =*q;
+    *q=*t;
+    *t=n;
     }
- }
-float cw=(float)ch/w;
-float ws=(float)w/sent;
-float r=(4.71*cw)+(0.5*ws)-21.43;
-read=ceil(r);
-if(read==1)
- { return "Kindergarten"; }
-else if(read==2)
- {return "First/Second Grade";}
-else if(read==3)
- {return "Third Grade";}
-else if(read==4)
- {return "Fourth Grade";}
-else if(read==5)
- {return "Fifth Grade";}
-else if(read==6)
- {return "Sixth Grade";}
-else if(read==7)
- {return "Seventh Grade";}
-else if(read==8)
- {return "Eighth Grade";}
-else if(read==9)
- {return "Ninth Grade";}
-else if(read==10)
- {return "Tenth Grade";}
-else if(read==11)
- {return "Eleventh Grade";}
-else if(read==12)
- {return "Twelfth Grade";}
-else if(read==13)
- {return "College student";}
-else if(read==14)
- {return "Professor";}
-}
+  void ssort(int m[],int l){
+   for(int i=0;i<l;i++)
+    swap_max(m,l,i);
+   }
